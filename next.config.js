8bde297a652c06a038d26382ffc93be385c7fd55
George Atoyan <gatoyan@3pm.ai>
2024-11-15 01:59:55 +0400

    deployed website from video

--- next.config.js
+++ next.config.js
@@ -2,19 +2,11 @@
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true,
-    domains: ['jakondak.github.io']  // Add this
+    unoptimized: true
   },
   basePath: '/referral_radar',
-  assetPrefix: '/referral_radar',    // Remove trailing slash
-  trailingSlash: true,
-  webpack: (config) => {             // Add this webpack config
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false
-    };
-    return config;
-  },
+  assetPrefix: '/referral_radar/',  // Add this line
+  trailingSlash: true,              // Add this line
 }
 
 module.exports = nextConfig
