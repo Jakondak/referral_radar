2b866cd0a4045f2f4158b57dc3bfb8a90c832317
George Atoyan <gatoyan@3pm.ai>
2024-11-17 02:36:45 +0400

    update package.json

--- next.config.js
+++ next.config.js
@@ -4,9 +4,16 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: 'referral_radar',
-  assetPrefix: 'referral_radar/',  // Add this line
-  trailingSlash: true,              // Add this line
+  basePath: '/referral_radar',
+  assetPrefix: '/referral_radar', // Remove trailing slash
+  trailingSlash: true,
+  webpack: (config) => {          // Add webpack config
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
